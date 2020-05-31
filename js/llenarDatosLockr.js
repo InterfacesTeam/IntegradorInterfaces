449bec0107a6b289c8d647b766b23a322ac35342
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2020-05-31 17:13:51 -0300

    Vamo lo pibe

---
+++ js/llenarDatosLockr.js
@@ -0,0 +1,20 @@
+Lockr.set(1 , {
+    "ID": 1,
+    "Descripción": "Pedido de notebook",
+    "Estado": "Abierta",
+    "FechaSolicitud": "30/03/2020"
+});
+
+Lockr.set(2 , {
+    "ID": 2,
+    "Descripción": "Cambio de horario",
+    "Estado": "En progreso",
+    "FechaSolicitud": "03/04/2020"
+});
+
+Lockr.set(3 , {
+    "ID": 3,
+    "Descripción": "Pedido de una bicicleta para llegar a la universidad",
+    "Estado": "Cerrada",
+    "FechaSolicitud": "12/05/2020"
+});
--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -1,94 +1,33 @@
-window.solicitudes = [
-    {
-        "ID": 1,
-        "Descripción": "Pedido de notebook",
-        "Estado": "Abierta",
-        "FechaSolicitud": "30/03/2020"
-    },
-    {   
-        "ID": 2,
-        "Descripción": "Cambio de horario",
-        "Estado": "En progreso",
-        "FechaSolicitud": "03/04/2020"
-    },
-    {   
-        "ID": 3,
-        "Descripción": "Pedido de una bicicleta para llegar a la universidad",
-        "Estado": "Cerrada",
-        "FechaSolicitud": "12/05/2020"
-    }
-]
-mostrarJSON()
 
-
-
-function agregarSolicitud(nuevaDescripción, nuevaFecha, nuevoEstado){
-    var nuevaSolicitud = {
-        ID: solicitudes.length + 1,
-        Descripción: nuevaDescripción,
-        FechaSolicitud: nuevaFecha,
-        Estado: nuevoEstado
-    }
-
-    solicitudes.push(nuevaSolicitud);
-
-    crearSolicitudEnTabla()
-}
-
-function llenarSolicitud(){
-    var nuevaDescripción = document.getElementById("descripcion").value,
-    f = new Date();
-    nuevaFecha = (f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
-    nuevoEstado = "Abierto"
-
-    location.href="paginaPrincipal.html";
-    agregarSolicitud(nuevaDescripción, nuevaFecha, nuevoEstado);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+actualizarTablaSolicitudes()
 
 function actualizarTablaSolicitudes() {
+    var solicitudes = Lockr.getAll();
+
     tbody = document.querySelector('#tablaSolicitudes tbody');
     tbody.innerHTML = '';
 
-    for(var i = 0; i < window.solicitudes.length; i++){
+    for(var i = 0; i < solicitudes.length; i++){
         var fila = tbody.insertRow(i);
         var celdaDescripcion = fila.insertCell(0),
         celdaFecha = fila.insertCell(1),
         celdaEstado = fila.insertCell(2),
         celdaCheckBox = fila.insertCell(3);
-        
-        celdaDescripcion.innerHTML = window.solicitudes[i].Descripción;
-        celdaFecha.innerHTML = window.solicitudes[i].FechaSolicitud;
-        celdaEstado.innerHTML = window.solicitudes[i].Estado;
+
+        celdaDescripcion.innerHTML = solicitudes[i].Descripción;
+        celdaFecha.innerHTML = solicitudes[i].FechaSolicitud;
+        celdaEstado.innerHTML = solicitudes[i].Estado;
 
         var checkBox = document.createElement('input')
         checkBox.type ='radio';
         checkBox.className = "checkBox",
         checkBox.name = "boton";
-        checkBox.value = window.solicitudes[i].ID;
+        checkBox.value = solicitudes[i].ID;
 
         celdaCheckBox.appendChild(checkBox);
-
         tbody.appendChild(fila);
     }
 }
-
 function eliminarSolicitud(){
     var todosLosCheckBox = document.getElementsByClassName("checkBox");
     var IDCheckeado;
@@ -97,12 +36,6 @@ function eliminarSolicitud(){
             IDCheckeado = todosLosCheckBox[i].value
         }
     }
-    console.log(IDCheckeado)
-    window.solicitudes = window.solicitudes.filter( solicitud => solicitud.ID != IDCheckeado )
+    Lockr.rm(IDCheckeado)
     actualizarTablaSolicitudes()   
-}
-
-
-function mostrarJSON(){
-    console.log(window.solicitudes)
 }
