afffae9b9b164d5f61bf4f6c4922bcf0a2b9401f
GerezPablo <pablogerez2000@gmail.com>
2020-06-06 00:45:26 -0300

    Chance para un ultimo arreglo

--- js/login.js
+++ js/login.js
@@ -1,3 +1,26 @@
+Lockr.flush()
+
+Lockr.set(1 , {
+    "ID": 1,
+    "Descripción": "Pedido de notebook",
+    "Estado": "Abierta",
+    "FechaSolicitud": "30/03/2020"
+});
+
+Lockr.set(2 , {
+    "ID": 2,
+    "Descripción": "Cambio de horario",
+    "Estado": "En progreso",
+    "FechaSolicitud": "03/04/2020"
+});
+
+Lockr.set(3 , {
+    "ID": 3,
+    "Descripción": "Pedido de una bicicleta para llegar a la universidad",
+    "Estado": "Cerrada",
+    "FechaSolicitud": "12/05/2020"
+});
+
 function redireccionar(){
     document.getElementById("carga").style.display="block";
     setTimeout('validarLogin()', 3000);
